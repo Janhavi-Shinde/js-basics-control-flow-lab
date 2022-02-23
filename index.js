cd7a53cd51f01ae3b50cbba6451e771ad63b0421
janhavi <janashinde394@gmail.com>
2022-02-23 23:15:19 +1100

    completed lab successfully

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+let rideFeet; 
+let freeRide; 
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(rideFeet) {
+  if (rideFeet <= 400) {
+    return "This one is on me!";
+  } else if (rideFeet > 2500) {
+return "No can do.";
+    } else if (rideFeet > 2000) {
+   return "I will gladly take your thirty bucks.";
+    }
+ }
+scuberGreetingForFeet(199);
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+let city = "NYC";
+let nyc = "Ok, sounds good."
+let notNYC = "No go."
+function ternaryCheckCity(city){
+  return city === "NYC" ? nyc : notNYC;
+     } 
+  ternaryCheckCity ("Pittsburgh")
+
+  
+let message;
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+      case "generous": 
+        return message = "Thank you so much.";
+        break; 
+      case "not as generous":
+        return message = "Thank you.";
+        break;
+        default: return "Bye."; 
+        break;
+        }  
+  }
+
+switchOnCharmFromTip("generous");
+
+
